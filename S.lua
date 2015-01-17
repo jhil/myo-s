1b99b9b628b8fa068a011be24ab2aea4eb06185d
Jeff Hilnbrand <jahilnbrand@gmail.com>
2015-01-17 15:16:15 -0500

    set onFist

--- S.lua
+++ S.lua
@@ -2,13 +2,23 @@ scriptId = 'com.bgoti.s'
 scriptTitle = "S"
 scriptDetailsUrl = ""
 
-function onPoseEdge(pose, edge)	
-    if (pose ~= "rest" and pose ~= "unknown") then
-        myo.unlock(edge == "off" and "timed" or "hold")
+function onPoseEdge(pose, edge)
+    myo.setLockingPolicy("none")
+    
+    local keyEdge = edge == "off" and "up" or "down"
+    
+    if (pose == "waveOut") then
+        onWaveOut(keyEdge)      
+    elseif (pose == "waveIn") then
+        onWaveIn(keyEdge)
+    elseif (pose == "fist") then
+        onFist(keyEdge)
+    elseif (pose == "fingersSpread") then
+        onFingersSpread(keyEdge)            
     end
 end
- 
+
 function onFist(keyEdge)
-    myo.debug("S")
+    myo.debug("S")    
     myo.keyboard("s",keyEdge)
 end
